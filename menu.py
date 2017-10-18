c6dc9f61b847d84ced046c09141dbac1cf330873
Stefan Ronacher <stefan.ronacher.78@gmail.com>
2017-10-18 16:51:39 -0300

    con el menu

--- menu.py
+++ menu.py
@@ -3,14 +3,14 @@ import pilasengine
 #import jugar
 
 class Jugador(pilasengine.actores.Actor):
-    def iniciar(self,nombre):
-        self.imagen = "imagenes/"+nombre+".jpg"
+    def iniciar(self):
+        self.imagenpers = "imagenes/"+nombre+".jpg"
         self.escala = 0.1
 
 
 class Proyectil(pilasengine.actores.Actor):
     def iniciar(self,nombre):
-        self.imagen = "imagenes/proyectil_"+nombre+".jpg"
+        self.imagenproy = "imagenes/proyectil_"+nombre+".jpg"
 
 
 class Jugar(pilasengine.escenas.Escena):
@@ -19,46 +19,47 @@ class Jugar(pilasengine.escenas.Escena):
         print ("A jugar "+nombre+"!!!")
         #ArrancarJuego(nombre)
         pilas.actores.vincular(Proyectil)
+        print("jugar1")
         pilas.actores.vincular(Jugador)
-        jugador = Jugador(pilas,nombre)
+        print("jugar2")
+        jugador = Jugador(pilas)
 
-        #jugador.aprender(self.habilidades.Disparar,
-        #                 municion="Proyectil",
-        #                 )
-
-        #jugador.aprender("moverseComoCoche")
+        proyectil = Proyectil(pilas)
+        print("jugar3")
+        jugador.aprender(self.habilidades.Disparar,
+                         municion="Proyectil",
+                         )
+        print("jugar4")
+        jugador.aprender("moverseComoCoche")
         pass
+
 
     def ejecutar(self):
         pass
 
-def ArrancarJuego(nombre):
-    pilas.actores.vincular(Proyectil)
 
-def QuienJuega(nombre):
-    print("nombrearg "+ nombre)
-    print( "banda1" )
-    #pilas.actores.vincular(Jugador)
-    print( "banda2")
-    #jugador = Jugador(pilas)
-    print( "banda3")
-    #jugador.nombre = nombre
-    print( "banda4")
-    #print( "jugador.nombre: "+jugador.nombre)
-    pilas.escenas.vincular(Jugar)
-    print("banda5")
-    pilas.escenas.Jugar(nombre)
-    print("banda6")
-    pass
+#class Jugador(pilasengine.actores.Actor):
+    #nombre = "nombrejugador"
 
-
-class Jugador(pilasengine.actores.Actor):
-    nombre = "nombrejugador"
-
-    def iniciar(self):
-        print( "JUgador.iniciar")
+    #def iniciar(self):
+    #    print( "JUgador.iniciar")
 
 class SeleccionJugador(pilasengine.escenas.Escena):
+    def QuienJuega(self,nombre):
+        print("nombrearg " + nombre)
+        print("banda1")
+        # pilas.actores.vincular(Jugador)
+        print("banda2")
+        # jugador = Jugador(pilas)
+        print("banda3")
+        # jugador.nombre = nombre
+        print("banda4")
+        # print( "jugador.nombre: "+jugador.nombre)
+        #pilas.escenas.vincular(Jugar)
+        print("banda5")
+        #pilas.escenas.Jugar(nombre)
+        print("banda6")
+        pass
 
     def iniciar(self):
         #pilas.fondos.Color(pilas.colores.negro)
@@ -71,10 +72,10 @@ class SeleccionJugador(pilasengine.escenas.Escena):
 
         pilas.actores.Menu(
             [
-                ('rama', QuienJuega('rama')),
-                #('stefan', QuienJuega('stefan')),
-                #('fede', QuienJuega('fede')),
-                #('nacho', QuienJuega('nacho')),
+                ('rama', self.QuienJuega('rama')),
+                ('stefan', self.QuienJuega('stefan')),
+                ('fede', self.QuienJuega('fede')),
+                ('nacho', self.QuienJuega('nacho')),
             ])
         pass
 
@@ -93,7 +94,7 @@ class PantallaBienvenida(pilasengine.escenas.Escena):
         texto_menu.y = 200
         texto_menu.color = pilas.colores.Color(255, 0, 0, 0)
         texto_menu.escala = [1, 4.5]
-        texto_menu.rotacion = [0, 360 * 3]
+        #texto_menu.rotacion = [0, 360 * 3]
         opcionesmenuppal = \
             [
                 ('iniciar juego', iniciar_juego),
